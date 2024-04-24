a6a53aa5d01a4cdfdc212b5874013a04114ed80a
FarizieFar <aalfarizi688@gmail.com>
2024-04-24 14:50:38 +0700

    add tagihanair

---
+++ src/app/tagihanair/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Page from ".";
+
+export const metadata = {
+  title: "Cek Tagihan",
+  description:
+    "Easily check your water bills and usage on our dashboard. Enter your customer ID to view your total charges and manage your account settings.",
+  //ogTitle:'...'
+};
+
+export default function TagihanAirPage() {
+  return <Page />;
+}
